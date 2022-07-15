936303c499f1cd06d3222dba6e0b707c9c6ee3b6
Brandon Reyes <brandon.reyesmi@gmail.com>
2022-07-15 11:29:15 -0500

    feat: add meta tags and image banner

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,14 +7,23 @@ type Props = {
   title?: string;
 };
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
       <Head>
         <title>{title || "Pokemon App"}</title>
         <meta name="author" content="Brandon Reyes" />
-        <meta name="description" content="Información sobre el pokémon XXXXX" />
-        <meta name="keywords" content="XXXX, pokemon, pokedex" />
+        <meta name="description" content={`Información sobre ${title}`} />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta se la página sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
       <Navbar />
       <main
